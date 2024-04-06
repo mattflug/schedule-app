39df41cc36f511c81c87936edcb64d8a90516e94
Ghost <mbflug@gmail.com>
2024-04-06 14:37:54 -0600

    updating icecream app

--- demo/demo/src/main/java/com/schedule/demo/model/PersonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.schedule.demo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PersonRepo extends JpaRepository<Person, Long> {
-}
